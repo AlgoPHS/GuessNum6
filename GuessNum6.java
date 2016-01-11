fc6a41a1f232bffe8bb306f167f982ab9ffb2da9
Alex Chen <AlexanderKChen@gmail.com>
2016-01-11 12:54:16 -0500

    finished decision method

--- GuessNum6.java
+++ GuessNum6.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
 public class GuessNum6 {
+    public static String decision(int x, int r) {
+	if(x == r)
+	    return "Excellent! You guessed the number! You guessed in 4 tries.Would you like to play again (y/n)?";
+	else if(x < r)
+	    return "Too low. Try again.";
+	else
+	    return "Too high. Try again.";
+    }
   public static void main(String[]args) {
   }
 }

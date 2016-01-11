9ce2e0623ddb1a04b161b9239e908164d5500613
marthacryan <martha.cryan326@gmail.com>
2016-01-11 13:02:31 -0500

    Made the main method

--- GuessNum6.java
+++ GuessNum6.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class GuessNum6 {
-  public static void main(String[]args) {
-  }
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner();
+        do {
+            int num = Math.Random.nextInt(100)+1;
+            int count = 0;
+            String ret;
+            do {
+                int guess = scanner.nextInt();
+                count++;
+                int response = decision(guess, num);
+                if (response == 0) ret = "Excellent! You guessed the number! You guessed in "+count+" tries. Would you like to play again (y/n)?";
+                else if (response > 0) ret = "Too high. Try again.";
+                else ret = "Too low. Try again.";
+            } while (response != "Excellent! You guessed the number! You guessed in "+count+" tries. Would you like to play again (y/n)?");
+            String yesorno = scanner.next();
+        } while (yesorno == "y");
+        
+    }
 }
